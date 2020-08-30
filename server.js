58eea82715ce5b9f27cdc246b429ff71c90e993a
arisbudi31 <arisjoharie@gmail.com>
2020-08-30 23:18:04 +0700

    menambah get route pertama

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+
+const mockUserData = [
+    { name: 'Aris' },
+    { name: 'Budi' }
+]
+
+app.get('/users', (req, res) => {
+    res.json({
+        success: true,
+        message: 'succes mendapatkan data user, nice!',
+        users: mockUserData
+    })
+})
 app.listen(8000, () => {
-    console.log("server is running");
+    console.log("server is listening");
 })
