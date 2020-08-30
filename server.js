041f3dcef272ca0ca5af3eb791931b3827ce602e
arisbudi31 <arisjoharie@gmail.com>
2020-08-30 23:59:43 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const mockUserData = [
     { name: 'Aris' },
@@ -21,6 +23,31 @@ app.get('/users/:id', (req, res) => {
         message: 'got one user',
         user: req.params.id
     })
+})
+
+app.post('/login', (req, res) => {
+    //data yang didapat dari input user
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //didapat dari databae
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        // In practice, use JSON web token sign method here to make an encrypted token
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+
 })
 app.listen(8000, () => {
     console.log("server is listening");
